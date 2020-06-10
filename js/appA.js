a549efd6bf55ef416d3aa930271708a6efa965c2
henryennui <62828663+henryennui@users.noreply.github.com>
2020-06-10 00:43:14 -0700

    all functional requirements finished
    just need to add css styles then im done woo!

---
+++ js/appA.js
@@ -0,0 +1,78 @@
+function renderBooks(book_list) {
+  
+    //select the <tbody> element
+    //you can make this more precise by using a descendant selector,
+    //referring to a particular <table> by ID or style class name
+    var tbody = document.querySelector(".books tbody");
+  
+    //clear any existing content in the body
+    tbody.textContent = "";
+  
+    // for each book
+    for (var i = 0; i < book_list.length; i++) {
+      // render the book row
+      var row = renderBook(book_list[i]);
+      // append it to the table
+      tbody.appendChild(row);
+    }
+  }
+  
+  function renderBook(book) {
+    //create the <tr> element
+    var tr = document.createElement("tr");
+  
+    //create and append the <td> elements
+    tr.appendChild(renderBookProp(book.title, true));
+    tr.appendChild(renderBookProp(book.author, true));
+    tr.appendChild(renderBookProp(book.topics, true));
+  
+    //return the table row to the caller
+    return tr;
+  }
+  
+  function renderBookProp(content, nonNumeric) {
+    //create the new <td> element
+    var td = document.createElement("td");
+  
+    //set its text content to the provided value
+    td.textContent = content;
+  
+    //if it should be formatted as numeric...
+    if (nonNumeric) {
+      //add the "numeric" style class
+      td.classList.add("non-numeric");
+    }
+  
+    //return the new element to the caller
+    return td;
+  }
+  
+  var searchInput = document.getElementById("book-filter");
+  
+  // Should this book be in our list?
+  function isBookFound(book) {
+    // Get the user input
+    var userInput = searchInput.value;
+  
+    // Make the input lower case
+    var lowercaseUserInput = userInput.toLowerCase();
+  
+    // Make the book topics lowercase
+    var lowercaseTopics = book.topics.toLowerCase();
+  
+    // Check if the user input is in the lowercase book topics
+    if (lowercaseTopics.indexOf(lowercaseUserInput) >= 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  // Listen for when a user types in the filter field
+  searchInput.addEventListener("input", function () {
+    // Find any books that match the user input
+    var filtered_books = themeA.filter(isBookFound);
+  
+    // Update the book table with the new list
+    renderBooks(filtered_books);
+  });
---
+++ js/themearrays.js
@@ -0,0 +1,191 @@
+// community 
+const themeA = [
+    {
+      "title": "Daniel Has A Good Day",
+      "author": "Archer, Micha",
+      "topics": "neighborhood, different perspectives",
+      "year": "2019"   
+    },
+    {
+      "title": "Carmela Full of Wishes",
+      "author": "de la Peña, Matt",
+      "topics": "immigration, change, family",
+      "year": "2018" 
+    },
+    {
+      "title": "Last Stop on Market Street",
+      "author": "de la Peña, Matt",
+      "topics": "intergenerational, family, resilience",
+      "year": "2015" 
+    },
+    {
+      "title": "What Can a Citizen Do?",
+      "author": "Eggers, Dave",
+      "topics": "civic engagement, service, change",
+      "year": "2018" 
+    },
+    {
+      "title": "My Papi Has a Motorcycle",
+      "author": "Quintero, Isabel",
+      "topics": "resilience, change, poverty",
+      "year": "2019" 
+    },
+    {
+      "title": "5 O'clock Band, The",
+      "author": "Andrews, Troy",
+      "topics": "different perspectives, neighborhood",
+      "year": "2018" 
+    },
+    {
+      "title": "Birdsong",
+      "author": "Flett, Julie",
+      "topics": "intergenerational, grief, loneliness",
+      "year": "2019" 
+    },
+    {
+      "title": "Fry Bread: A Native American Family Story",
+      "author": "Maillard, Kevin Noble",
+      "topics": "tradition, culture, intergenerational",
+      "year": "2019" 
+    },
+    {
+      "title": "Islandborn",
+      "author": "Díaz, Junot",
+      "topics": "assumptions, culture, immigration",
+      "year": "2018" 
+    },
+    {
+      "title": "I Can Write the World",
+      "author": "Sanders, Joshunda",
+      "topics": "journalism, assumptions, resilience",
+      "year": "2019" 
+    }
+];
+
+// gender diversity
+const themeB = [
+  {
+      "title": "Julián Is a Mermaid",
+      "author": "Love, Jessica",
+      "topics": "supportive family, intergenerational, play, affirmation",
+      "year": "2018"    
+    },
+    {
+      "title": "Boy and the Bindi, The",
+      "author": "Shraya, Vivek",
+      "topics": "supportive family, gender roles, culture",
+      "year": "2016" 
+    },
+    {
+      "title": "I Love My Purse",
+      "author": "DeMont, Belle",
+      "topics": "gender roles, expression, challenge",
+      "year": "2017" 
+    },
+    {
+      "title": "Just Ask!: Be Different, Be Brave, Be You",
+      "author": "Sotomayor, Sonia",
+      "topics": "differences, disability, communication",
+      "year": "2019" 
+    },
+    {
+      "title": "They She He Me: Free to Be!",
+      "author": "Gonzales, Maya",
+      "topics": "pronouns, language, ambiguity, exploration",
+      "year": "2017" 
+    },
+    {
+      "title": "From the Stars in the Sky to the Fish in the Sea",
+      "author": "Thom, Kai Cheng",
+      "topics": "fantasy, transformation, ambiguity",
+      "year": "2017" 
+    },
+    {
+      "title": "Witch Boy, The",
+      "author": "Ostertag, Molly",
+      "topics": "fantasy, allegory, gender roles",
+      "year": "2017" 
+    },
+    {
+      "title": "It Feels Good to Be Yourself: A Book about Gender Identity",
+      "author": "Thorn, Theresa",
+      "topics": "gender identity, exploration, language",
+      "year": "2019" 
+    },
+    {
+      "title": "Neither",
+      "author": "Anderson, Airlie",
+      "topics": "ambiguity, differences, identity",
+      "year": "2018" 
+    },
+    {
+      "title": "Call Me Tree: Llámame árbol",
+      "author": "González, Maya Christina",
+      "topics": "nature, belonging, exploration, development",
+      "year": "2014" 
+    },
+];
+
+// emotions
+const themeC = [
+  {
+      "title": "When Sophie Gets Angry-- Really, Really Angry",
+      "author": "Bang, Molly",
+      "topics": "outburst, conflict, nature, escape",
+      "year": "1999"    
+    },
+    {
+      "title": "Wild Book, The",
+      "author": "Engle, Margarita",
+      "topics": "self-expression, disability, historical, culture",
+      "year": "2012" 
+    },
+    {
+      "title": "Invisible Boy, The",
+      "author": "Ludwig, Trudy",
+      "topics": "friendship, understanding",
+      "year": "2013" 
+    },
+    {
+      "title": "Who Wet My Pants?",
+      "author": "Shea, Bob",
+      "topics": "humor, embarrassment, shame, denial, forgiveness",
+      "year": "2019" 
+    },
+    {
+      "title": "Day You Begin, The",
+      "author": "Woodson, Jacqueline",
+      "topics": "bravery, differences, shyness",
+      "year": "2018" 
+    },
+    {
+      "title": "My Heart",
+      "author": "Luyken, Corinna",
+      "topics": "expression, understanding, visualization",
+      "year": "2019" 
+    },
+    {
+      "title": "Pilu of the Woods",
+      "author": "Nguyen, Mai K",
+      "topics": "forgiveness, grief, overwhelm, allegory, fantasy",
+      "year": "2019" 
+    },
+    {
+      "title": "Being Edie is Hard Today",
+      "author": "Brashares, Ben",
+      "topics": "self-regulation, connection, tolerance, mindfulness",
+      "year": "2019" 
+    },
+    {
+      "title": "My Mixed Emotions:  Help Your Kids Handle Their Feelings",
+      "author": "Healy, Maureen",
+      "topics": "self-regulation, tolerance, mindfulness",
+      "year": "2018" 
+    },
+    {
+      "title": "What Were You Thinking?: Learning to Control Your Impulses",
+      "author": "Smith, Bryan",
+      "topics": "humor, reflection, tolerance, self-regulation",
+      "year": "2016" 
+    }
+];
